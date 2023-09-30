69d366cf63cbca58bfd806d7b214d0e61135bc10
Parth_Ghatge <parthghatge982@gmail.com>
2023-09-30 21:02:09 +0530

    Mail Verify Feature Part 1.

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,4 +1,5 @@
 import { connect } from "@/dbConfig/dbConfig";
+import { sendEmail } from "@/helpers/mailer";
 import User from "@/models/user.model";
 import bcryptjs from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
@@ -23,6 +24,9 @@ export async function POST(request: NextRequest) {
         })
 
         const savedUser = await newUser.save();
+
+        //send verification email
+        await sendEmail({ email, emailType: "VERIFY", userId: savedUser._id })
 
         return NextResponse.json({
             message: "User Created Successfully",
---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+
+export default function VerifyEmailPage() {
+  const [token, setToken] = useState("");
+  const [verified, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.reponse.data);
+    }
+  };
+}
